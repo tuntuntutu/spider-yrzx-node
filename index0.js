7771088dfbebe4f608909906635683dff31e09af
qiushengqiang <qsq11170923@163.com>
2018-08-12 14:04:13 +0800

    285话之后切换网站源

--- index0.js
+++ index0.js
@@ -4,21 +4,20 @@ var colors = require("colors");
 
 
 
-const host = 'http://images.lancaier.com/';
+const host = 'http://pic.fxdm.cc/tu/undefined/%E4%B8%80%E4%BA%BA%E4%B9%8B%E4%B8%8B';
 // todo
-const filePath = '/Users/qiu/pictures/yrzx/';
-const startIndex = 259;
-const endIndex = 303; // 可以打开网站看一下，303 对应 284话
-
+const filePath = '/Users/qiu/pictures/284-330/'
+const startIndex = 364684;
+const endIndex = 364731;
 
 var downloadImgC = new Crawler({
-    maxConnections : 2,
-    rateLimit: 5000,
+    maxConnections : 4,
+    rateLimit: 2000,
     encoding:null,
     jQuery:false,// set false to suppress warning message.
     callback:function(err, res, done){
         if(err){
-            console.error(err.stack.error);
+            console.log(err.stack.error);
         }else{
             fs.createWriteStream(res.options.fileName).write(res.body);
             console.log(`文件存储在${res.options.fileName}`.green)
@@ -30,28 +29,32 @@ var downloadImgC = new Crawler({
  // downloadImgC.queue({"fileName": filePath + "一人之下235-16.jpg","uri":"http://img21.mtime.cn/CMS/Gallery/2011/07/02/170837.97014036_160X160.jpg"});
 
 var c = new Crawler({
-    maxConnections : 1,
-    rateLimit: 10000,
+    maxConnections : 2,
+    rateLimit: 4000,
     // This will be called for each crawled page
     callback : function (error, res, done) {
         if(error){
-            console.error(error);
+            console.log(error.red);
         }else{
           try {
-            // 这几步比较关键，需要分析网页如何动态生成图片地址。如果理解了原理就能明白下面几行代码
+            // 网站为了防爬，通过闭包中 eval 执行 一段 一定规则的正则替换生成图片相关信息，对外暴露 cInfo 全局变量，然后通过 core.js去解析，再去请求真实图片，替换loading动画。
             const $ = res.$;
-            const script = $('#pager').parent().siblings('script')[0].children[0].data.match(/eval.*/)[0];
+            const script = $('.mtb4').siblings('script')[0].children[0].data;
+
             eval(script);
 
-            const imgs = cInfo.fs
-            const picName = $("title").text().split('-')[0];
+            console.log(cInfo.files)
+            const imgs = cInfo.files;
+
+            const picName = $("title").text().split(' ')[0].match(/\d+/)[0];
+
 
             imgs.forEach((item, index)=>{
-              const matchList = item.match(/\/(\d+).jpg/) // 屏蔽每章节的广告图片
-              if(!matchList || isNaN(Number(matchList[1]))) return
-              var img = { fileName: `${filePath}${picName}-${index + 1}.jpg`, uri: `${host}${item}`}
+              if(!item) return
+              var img = { fileName: `${filePath}${picName}-${index + 1}.jpg`, uri: `${host}/${picName}/${item}`}
 
               console.log(`开始下载：${JSON.stringify(img)}`.green);
+
               downloadImgC.queue(img);
             })
           }catch(e){
@@ -63,11 +66,12 @@ var c = new Crawler({
     }
 });
 
-// 网站URL 259后缀 对应 235话，我看到 235，所以就从这边开始了
+// 364730
 for(let i = startIndex; i < endIndex; i++ ){
-  const index = `0${i}`
+  const uri = `http://www.omanhua.com/comic/17521/${i}/`
+  console.log(`发起请求：${uri}`.green);
   c.queue([{
-      uri: `http://www.57mh.com/27885/${index}.html`,
+      uri: uri,
       jQuery: true
   }]);
 }
